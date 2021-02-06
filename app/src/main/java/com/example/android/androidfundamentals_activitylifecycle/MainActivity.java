05fdd4d823a5ca626bc99c151bfcb01d09ae9029
ahmed-razic <Ibrahim2012*>
2021-02-06 11:47:56 +0100

    added second activity

--- app/src/main/java/com/example/android/androidfundamentals_activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/android/androidfundamentals_activitylifecycle/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.android.androidfundamentals_helloconstraint;
+package com.example.android.androidfundamentals_activitylifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
--- app/src/test/java/com/example/android/androidfundamentals_activitylifecycle/ExampleUnitTest.java
+++ app/src/test/java/com/example/android/androidfundamentals_activitylifecycle/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.android.androidfundamentals_helloconstraint;
+package com.example.android.androidfundamentals_activitylifecycle;
 
 import org.junit.Test;
 

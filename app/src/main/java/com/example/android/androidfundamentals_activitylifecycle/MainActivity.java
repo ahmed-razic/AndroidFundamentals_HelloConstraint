45e2507bf1255e06224221b83877c8d8748f56be
ahmed-razic <Ibrahim2012*>
2021-02-06 12:34:05 +0100

    added lifecycle methods

--- app/src/main/java/com/example/android/androidfundamentals_activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/android/androidfundamentals_activitylifecycle/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.android.androidfundamentals_activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,7 +13,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int mCountUp;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private int mCount;
     private TextView mShowCount;
     private Button mZeroButton;
     private Button mCountButton;
@@ -21,20 +24,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "--------");
+        Log.d(LOG_TAG, "onCreate()");
 
         mShowCount = findViewById(R.id.textView_count);
         mZeroButton = findViewById(R.id.button_zero);
         mCountButton = findViewById(R.id.button_count);
     }
 
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this, "Show toast text", Toast.LENGTH_LONG);
-        toast.show();
+    public void showHello(View view) {
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("KEY", mCount);
+        startActivity(intent);
     }
 
     public void resetToZero(View view) {
-        mCountUp = 0;
-        mShowCount.setText(String.valueOf(mCountUp));
+        mCount = 0;
+        mShowCount.setText(String.valueOf(mCount));
 
         view.setBackgroundColor(ContextCompat.getColor(this, R.color.gray));
         mCountButton.setBackgroundColor(ContextCompat.getColor(this, R.color.blue));
@@ -42,14 +49,50 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
 
-        mCountUp++;
-        mShowCount.setText(String.valueOf(mCountUp));
+        mCount++;
+        mShowCount.setText(String.valueOf(mCount));
 
-        if ( mCountUp % 2 == 0 ) {
+        if ( mCount % 2 == 0 ) {
             view.setBackgroundColor(ContextCompat.getColor(this, R.color.blue));
         } else {
             view.setBackgroundColor(ContextCompat.getColor(this, R.color.red));
         }
         mZeroButton.setBackgroundColor(ContextCompat.getColor(this, R.color.green));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy()");
     }
 }
--- app/src/main/java/com/example/android/androidfundamentals_activitylifecycle/SecondActivity.java
+++ app/src/main/java/com/example/android/androidfundamentals_activitylifecycle/SecondActivity.java
@@ -1,14 +1,74 @@
 package com.example.android.androidfundamentals_activitylifecycle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Log.d(LOG_TAG, "--------");
+        Log.d(LOG_TAG, "onCreate()");
+
+
+
+        TextView countTextView = findViewById(R.id.count_textView);
+        Intent intent = getIntent();
+        int count = intent.getIntExtra("KEY", 0);
+
+        countTextView.setText(String.valueOf(count));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy()");
     }
 }
+
+
+
+
